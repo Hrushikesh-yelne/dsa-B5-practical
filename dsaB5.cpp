5056434a16b85fbe197199320970fc2643e9dc6d
Hrushikesh-yelne <157500365+Hrushikesh-yelne@users.noreply.github.com>
2025-01-19 19:19:05 +0530

    Update dsaB5.cpp

--- dsaB5.cpp
+++ dsaB5.cpp
@@ -18,72 +18,72 @@ struct node{
 
 class GT{
 
-	public:
-		GT(){
-			root = NULL;
-		}
+public:
+	GT(){
+		root = NULL;
+	}
 		
-		string lbel;
-		int count;
+	string lbel;
+	int count;
 		
-		void create(){
-				root = new node;
+	void create(){
+		root = new node;
 				
-				cout<<"Name of the book:\t";
-				cin>>root->label;
-				cout<<"Number of chapters:\t";
-				cin>>root->ch_count;
+		cout<<"Name of the book:\t";
+		cin>>root->label;
+		cout<<"Number of chapters:\t";
+		cin>>root->ch_count;
 				
-				for(int i=0;i<root->ch_count;i++){
+		for(int i=0;i<root->ch_count;i++){
 				
-					root->child[i] = new node;
-					cout<<"Name of chapter "<< i+1 <<":\t";
-					cin>>root->child[i]->label;
-					cout<<"Number of sections:\t";
-					cin>>root->child[i]->ch_count;
+			root->child[i] = new node;
+			cout<<"Name of chapter "<< i+1 <<":\t";
+			cin>>root->child[i]->label;
+			cout<<"Number of sections:\t";
+			cin>>root->child[i]->ch_count;
 
-					for(int j=0;j<root->child[i]->ch_count;j++){
-						root->child[i]->child[j] = new node;
-						cout<<"Name of section "<< i+1 <<" - "<< j+1<< ":\t";
-						cin>>root->child[i]->child[j]->label;
-						cout<<"Number of sub-sections:\t";
-						cin>>root->child[i]->child[j]->ch_count;
+		for(int j=0;j<root->child[i]->ch_count;j++){
+			root->child[i]->child[j] = new node;
+			cout<<"Name of section "<< i+1 <<" - "<< j+1<< ":\t";
+			cin>>root->child[i]->child[j]->label;
+			cout<<"Number of sub-sections:\t";
+			cin>>root->child[i]->child[j]->ch_count;
 						
-						for(int k=0;k<root->child[i]->child[j]->ch_count;k++){
-							root->child[i]->child[j]->child[k] = new node;
-							cout<<"Name of sub-section "<< i+1 <<" - "<< j+1<< " - "<< k+1<< ":\t";
-							cin>>root->child[i]->child[j]->label;
+		for(int k=0;k<root->child[i]->child[j]->ch_count;k++){
+			root->child[i]->child[j]->child[k] = new node;
+			cout<<"Name of sub-section "<< i+1 <<" - "<< j+1<< " - "<< k+1<< ":\t";
+			cin>>root->child[i]->child[j]->label;
 
-						}
 					}
 				}
-		} 
+			}
+	} 
 		
 		void display(node * r){
-				cout<<endl<<"Name of book:\t";
-				cout<<root->label<<endl;
-				cout<<endl<<"Number of chapters:\t";
-				cout<<root->ch_count<<endl;
-			
-				for(int i=0;i<root->ch_count;i++){
-						cout<<endl<<"Name of chapter:\t";
-						cout<<root->child[i]->label<<endl;
-						cout<<endl<<"Number of sections:\t";
-						cout<<root->child[i]->ch_count<<endl;
-
-						for(int j=0;j<root->child[i]->ch_count;j++){
-							cout<<"\t\t"<< i+1 <<" - "<< j+1<< " Name of sections: ";
-							cout<<root->child[i]->child[j]->label<<endl;
-							cout<<"\t\tNumber of sub-section: ";
-							cout<<root->child[i]->child[j]->ch_count<<endl;
-							
-							for(int k=0;k<root->child[i]->child[j]->ch_count;k++){
-								cout<<"\t\t\t"<< i+1 <<" - "<< j+1<< " - "<< k+1<< " Name of sub-section: ";
-								cout<<root->child[i]->child[j]->label<<endl;
-							}
-						}
+			cout<<endl<<"Name of book:\t";
+			cout<<root->label<<endl;
+			cout<<endl<<"Number of chapters:\t";
+			cout<<root->ch_count<<endl;
+		
+		for(int i=0;i<root->ch_count;i++){
+				cout<<endl<<"Name of chapter:\t";
+				cout<<root->child[i]->label<<endl;
+				cout<<endl<<"Number of sections:\t";
+				cout<<root->child[i]->ch_count<<endl;
+				
+		for(int j=0;j<root->child[i]->ch_count;j++){
+				cout<<"\t\t"<< i+1 <<" - "<< j+1<< " Name of sections: ";
+				cout<<root->child[i]->child[j]->label<<endl;
+				cout<<"\t\tNumber of sub-section: ";
+				cout<<root->child[i]->child[j]->ch_count<<endl;
+						
+		for(int k=0;k<root->child[i]->child[j]->ch_count;k++){
+				cout<<"\t\t\t"<< i+1 <<" - "<< j+1<< " - "<< k+1<< " Name of sub-section: ";
+				cout<<root->child[i]->child[j]->label<<endl;
+				}
 			}
 		}
+	}
 };
 
 
